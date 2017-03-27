b975ae4cfac4ea537b142571f44e87cc35e5f1b0
Yu Xuan <yuxuan@livepay.my>
2017-03-27 17:01:05 +0800

    backup

--- ios/firebase/AppDelegate.m
+++ ios/firebase/AppDelegate.m
@@ -12,6 +12,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import Firebase;
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -31,6 +32,8 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  [FIRApp configure];
   return YES;
 }
 
